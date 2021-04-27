82b6bf26ac31dc1423b09abaaf2b3e979842eb39
Stefano Balocco <stefano.balocco@gmail.com>
2021-04-27 23:54:29 +0200

    Fixed a bug in implicit self closing tags (thank you for the headache, html5)

--- jTDAL.ts
+++ jTDAL.ts
@@ -201,6 +201,9 @@ namespace jTDAL {
 						closingPosition = [ tmpMatch[ 'index' ], tmpMatch[ 0 ].length ];
 					}
 				}
+				if( 'undefined' === typeof closingPosition[ 0 ] ) {
+					selfClosed = true;
+				}
 			}
 			// 0: js before tag open
 			// 1: tagOpen
@@ -211,131 +214,130 @@ namespace jTDAL {
 			// 6: tagClose
 			// 7: js after tag close
 			let current = [ '', tmpTDALTags[ 0 ], '', '', '', '', '', '' ];
-			if( 'undefined' !== typeof closingPosition[ 0 ] ) {
+			if( !selfClosed ) {
 				current[ 4 ] += Parse( template.substr( 0, closingPosition[ 0 ] ) );
 				current[ 6 ] += template.substr( closingPosition[ 0 ], closingPosition[ 1 ] );
 			}
 			tdal:
 			{
-				if( selfClosed || ( 'undefined' !== typeof closingPosition[ 0 ] ) ) {
-					if( 'undefined' !== typeof closingPosition[ 0 ] ) {
-						template = template.substr( closingPosition[ 0 ] + closingPosition[ 1 ] );
-					}
-					if( attributes[ 'data-tdal-condition' ] && ( regexp[ 'condition' ].exec( attributes[ 'data-tdal-condition' ][ 3 ] ) ) ) {
-						let tmpValue = ExpressionResultToBoolean( ParsePath( attributes[ 'data-tdal-condition' ][ 3 ] ) );
-						if( 'false' == tmpValue ) {
-							// the tag (and it's content) should be removed
-							break tdal;
-						} else if( 'true' != tmpValue ) {
-							current[ 0 ] += '+(true===' + tmpValue + '?""';
-							current[ 7 ] = ':"")' + current[ 7 ];
-						}
-					}
-					let tmpTDALrules;
-					if( attributes[ 'data-tdal-repeat' ] && ( tmpTDALrules = regexp[ 'repeat' ].exec( attributes[ 'data-tdal-repeat' ][ 3 ] ) ) ) {
-						let tmpValue = ParsePath( tmpTDALrules[ 2 ] );
-						if( ( 'false' == tmpValue ) || ( '""' == tmpValue ) || ( 'true' == tmpValue ) ) {
-							// 0 repetition, same as condition false
-							break tdal;
-						} else {
-							current[ 0 ] += '+(';
-							// current i = object
-							current[ 0 ] += 'false!==(t[i]=' + tmpValue + ')&&';
-							current[ 0 ] += '(!Array.isArray(t[i])||(t[i]=Object.assign({},t[i])))&&';
-							// current i = index for object loop ( i+=1 )
-							current[ 0 ] += '("object"===typeof t[i]&&0<Object.keys(t[i++]).length)&&(t[i++]=1)';
-							current[ 0 ] += '?';
-							current[ 0 ] += 'Object.keys(t[i-2]).reduce(function(o,e){';
-							current[ 0 ] += 'r["' + tmpTDALrules[ 1 ] + '"]=t[i-2][e];';
-							current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]={};';
-							current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["index"]=e;';
-							// current i = free index ( i+=1 )
-							current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["number"]=t[i-1]++;';
-							current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["even"]=0==(r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["number"]%2);';
-							current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["odd"]=1==(r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["number"]%2);';
-							current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["first"]=1==r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["number"];';
-							current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["length"]=Object.keys(t[i-2]);';
-							current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["last"]=r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["length"]==r["REPEAT"]["' +
-															tmpTDALrules[ 1 ] + '"]["number"];';
-							current[ 0 ] += 'return o';
-							// resetting i
-							current[ 7 ] = ';},""):"")+((i-=2)?"":"")' + current[ 7 ];
-						}
-					}
-					if( attributes[ 'data-tdal-content' ] && ( tmpTDALrules = regexp[ 'content' ].exec( attributes[ 'data-tdal-content' ][ 3 ] ) ) ) {
-						let tmpValue = ParsePath( tmpTDALrules[ 2 ] );
-						if( 'false' == tmpValue ) {
-							current[ 4 ] = '';
-						} else if( 'true' != tmpValue ) {
-							let encoding = [ '', '' ];
-							if( 'structure' != tmpTDALrules[ 1 ] ) {
-								encoding[ 0 ] = '(new String(';
-								encoding[ 1 ] = ')).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;")';
-							}
-							current[ 3 ] += '+(false !== (t[i]=' + tmpValue + ')&&("string"===typeof t[i]||("number"===typeof t[i]&&!isNaN(t[i])))?' + encoding[ 0 ] +
-															't[i]' + encoding[ 1 ] + ':(true!==t[i]?"":""';
-							current[ 5 ] += '))';
-						}
-					} else if( attributes[ 'data-tdal-replace' ] && ( tmpTDALrules = regexp[ 'content' ].exec( attributes[ 'data-tdal-replace' ][ 3 ] ) ) ) {
-						let tmpValue = ParsePath( tmpTDALrules[ 2 ] );
-						if( 'false' == tmpValue ) {
-							current[ 1 ] = '';
-							current[ 4 ] = '';
-							current[ 6 ] = '';
-						} else if( 'true' != tmpValue ) {
-							let encoding = [ '', '' ];
-							if( 'structure' != tmpTDALrules[ 1 ] ) {
-								encoding[ 0 ] = '(new String(';
-								encoding[ 1 ] = ')).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;")';
-							}
-							current[ 0 ] += '+(false!==(t[i]=' + tmpValue + ')&&("string"===typeof t[i]||("number"===typeof t[i]&&!isNaN(t[i])))?' + encoding[ 0 ] +
-															't[i]' + encoding[ 1 ] + ':(true!==t[i]?"":""';
-							current[ 7 ] = '))' + current[ 7 ];
-						}
-					}
-					if( attributes[ 'data-tdal-attributes' ] && ( tmpTDALrules = regexp[ 'attributes' ].exec( attributes[ 'data-tdal-attributes' ][ 3 ] ) ) ) {
-						while( null !== tmpTDALrules ) {
-							let tmpValue = ParsePath( tmpTDALrules[ 2 ] );
-							if( "false" === tmpValue ) {
-								if( 'undefined' !== ( typeof attributes[ tmpTDALrules[ 1 ] ] ) ) {
-									current[ 1 ] = current[ 1 ].replace( new RegExp( '\\s*' + tmpTDALrules[ 1 ] + '(?:=([\'"]).*?\\1)?' ), '' );
-								}
-							} else if( "true" !== tmpValue ) {
-								current[ 2 ] += '+(false!==(t[i]=' + tmpValue + ')&&("string"===typeof t[i]||("number"===typeof t[i]&&!isNaN(t[i])))?" ' + tmpTDALrules[ 1 ] +
-																'=\\""+t[i]+"\\"":(true!==t[i]?"":"';
-								if( 'undefined' !== ( typeof attributes[ tmpTDALrules[ 1 ] ] ) ) {
-									current[ 1 ] = current[ 1 ].replace( new RegExp( '\\s*' + tmpTDALrules[ 1 ] + '(?:=([\'"]).*?\\1)?' ), '' );
-									current[ 2 ] += tmpTDALrules[ 1 ] + '"' +
-																	( ( ( 'undefined' !== ( typeof attributes[ tmpTDALrules[ 1 ] ][ 3 ] ) ) && ( '' != attributes[ tmpTDALrules[ 1 ] ][ 3 ] ) ) ? '+"="+' +
-																	JSON.stringify( String( attributes[ tmpTDALrules[ 1 ] ][ 2 ] + attributes[ tmpTDALrules[ 1 ] ][ 3 ] +
-																	attributes[ tmpTDALrules[ 1 ] ][ 2 ] ) ) : "" );
-								} else {
-									current[ 2 ] += '"';
-								}
-								current[ 2 ] += '))';
-							}
-							tmpTDALrules = regexp[ 'attributes' ].exec( attributes[ 'data-tdal-attributes' ][ 3 ] );
-						}
-					}
-					if( attributes[ 'data-tdal-omittag' ] && ( regexp[ 'condition' ].exec( attributes[ 'data-tdal-omittag' ][ 3 ] ) ) ) {
-						let tmpValue = ExpressionResultToBoolean( ParsePath( attributes[ 'data-tdal-omittag' ][ 3 ] ) );
-						if( 'true' == tmpValue ) {
-							current[ 1 ] = '';
-							current[ 6 ] = '';
-						} else if( 'false' != tmpValue ) {
-							current[ 0 ] += '+(' + tmpValue + '?"":""';
-							current[ 3 ] = ')' + current[ 3 ];
-							current[ 5 ] += '+(' + tmpValue + '?"":""';
-							current[ 7 ] = ')' + current[ 7 ];
-						}
+				if( !selfClosed ) {
+					template = template.substr( closingPosition[ 0 ] + closingPosition[ 1 ] );
+				}
+				if( attributes[ 'data-tdal-condition' ] && ( regexp[ 'condition' ].exec( attributes[ 'data-tdal-condition' ][ 3 ] ) ) ) {
+					let tmpValue = ExpressionResultToBoolean( ParsePath( attributes[ 'data-tdal-condition' ][ 3 ] ) );
+					if( 'false' == tmpValue ) {
+						// the tag (and it's content) should be removed
+						break tdal;
+					} else if( 'true' != tmpValue ) {
+						current[ 0 ] += '+(true===' + tmpValue + '?""';
+						current[ 7 ] = ':"")' + current[ 7 ];
 					}
 				}
+				let tmpTDALrules;
+				if( attributes[ 'data-tdal-repeat' ] && ( tmpTDALrules = regexp[ 'repeat' ].exec( attributes[ 'data-tdal-repeat' ][ 3 ] ) ) ) {
+					let tmpValue = ParsePath( tmpTDALrules[ 2 ] );
+					if( ( 'false' == tmpValue ) || ( '""' == tmpValue ) || ( 'true' == tmpValue ) ) {
+						// 0 repetition, same as condition false
+						break tdal;
+					} else {
+						current[ 0 ] += '+(';
+						// current i = object
+						current[ 0 ] += 'false!==(t[i]=' + tmpValue + ')&&';
+						current[ 0 ] += '(!Array.isArray(t[i])||(t[i]=Object.assign({},t[i])))&&';
+						// current i = index for object loop ( i+=1 )
+						current[ 0 ] += '("object"===typeof t[i]&&0<Object.keys(t[i++]).length)&&(t[i++]=1)';
+						current[ 0 ] += '?';
+						current[ 0 ] += 'Object.keys(t[i-2]).reduce(function(o,e){';
+						current[ 0 ] += 'r["' + tmpTDALrules[ 1 ] + '"]=t[i-2][e];';
+						current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]={};';
+						current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["index"]=e;';
+						// current i = free index ( i+=1 )
+						current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["number"]=t[i-1]++;';
+						current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["even"]=0==(r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["number"]%2);';
+						current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["odd"]=1==(r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["number"]%2);';
+						current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["first"]=1==r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["number"];';
+						current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["length"]=Object.keys(t[i-2]);';
+						current[ 0 ] += 'r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["last"]=r["REPEAT"]["' + tmpTDALrules[ 1 ] + '"]["length"]==r["REPEAT"]["' +
+														tmpTDALrules[ 1 ] + '"]["number"];';
+						current[ 0 ] += 'return o';
+						// resetting i
+						current[ 7 ] = ';},""):"")+((i-=2)?"":"")' + current[ 7 ];
+					}
+				}
+				if( attributes[ 'data-tdal-content' ] && ( tmpTDALrules = regexp[ 'content' ].exec( attributes[ 'data-tdal-content' ][ 3 ] ) ) ) {
+					let tmpValue = ParsePath( tmpTDALrules[ 2 ] );
+					if( 'false' == tmpValue ) {
+						current[ 4 ] = '';
+					} else if( 'true' != tmpValue ) {
+						let encoding = [ '', '' ];
+						if( 'structure' != tmpTDALrules[ 1 ] ) {
+							encoding[ 0 ] = '(new String(';
+							encoding[ 1 ] = ')).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;")';
+						}
+						current[ 3 ] += '+(false !== (t[i]=' + tmpValue + ')&&("string"===typeof t[i]||("number"===typeof t[i]&&!isNaN(t[i])))?' + encoding[ 0 ] +
+														't[i]' + encoding[ 1 ] + ':(true!==t[i]?"":""';
+						current[ 5 ] += '))';
+					}
+				} else if( attributes[ 'data-tdal-replace' ] && ( tmpTDALrules = regexp[ 'content' ].exec( attributes[ 'data-tdal-replace' ][ 3 ] ) ) ) {
+					let tmpValue = ParsePath( tmpTDALrules[ 2 ] );
+					if( 'false' == tmpValue ) {
+						current[ 1 ] = '';
+						current[ 4 ] = '';
+						current[ 6 ] = '';
+					} else if( 'true' != tmpValue ) {
+						let encoding = [ '', '' ];
+						if( 'structure' != tmpTDALrules[ 1 ] ) {
+							encoding[ 0 ] = '(new String(';
+							encoding[ 1 ] = ')).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;")';
+						}
+						current[ 0 ] += '+(false!==(t[i]=' + tmpValue + ')&&("string"===typeof t[i]||("number"===typeof t[i]&&!isNaN(t[i])))?' + encoding[ 0 ] +
+														't[i]' + encoding[ 1 ] + ':(true!==t[i]?"":""';
+						current[ 7 ] = '))' + current[ 7 ];
+					}
+				}
+				if( attributes[ 'data-tdal-attributes' ] && ( tmpTDALrules = regexp[ 'attributes' ].exec( attributes[ 'data-tdal-attributes' ][ 3 ] ) ) ) {
+					while( null !== tmpTDALrules ) {
+						let tmpValue = ParsePath( tmpTDALrules[ 2 ] );
+						if( "false" === tmpValue ) {
+							if( 'undefined' !== ( typeof attributes[ tmpTDALrules[ 1 ] ] ) ) {
+								current[ 1 ] = current[ 1 ].replace( new RegExp( '\\s*' + tmpTDALrules[ 1 ] + '(?:=([\'"]).*?\\1)?' ), '' );
+							}
+						} else if( "true" !== tmpValue ) {
+							current[ 2 ] += '+(false!==(t[i]=' + tmpValue + ')&&("string"===typeof t[i]||("number"===typeof t[i]&&!isNaN(t[i])))?" ' + tmpTDALrules[ 1 ] +
+															'=\\""+t[i]+"\\"":(true!==t[i]?"":"';
+							if( 'undefined' !== ( typeof attributes[ tmpTDALrules[ 1 ] ] ) ) {
+								current[ 1 ] = current[ 1 ].replace( new RegExp( '\\s*' + tmpTDALrules[ 1 ] + '(?:=([\'"]).*?\\1)?' ), '' );
+								current[ 2 ] += tmpTDALrules[ 1 ] + '"' +
+																( ( ( 'undefined' !== ( typeof attributes[ tmpTDALrules[ 1 ] ][ 3 ] ) ) && ( '' != attributes[ tmpTDALrules[ 1 ] ][ 3 ] ) ) ? '+"="+' +
+																JSON.stringify( String( attributes[ tmpTDALrules[ 1 ] ][ 2 ] + attributes[ tmpTDALrules[ 1 ] ][ 3 ] +
+																attributes[ tmpTDALrules[ 1 ] ][ 2 ] ) ) : "" );
+							} else {
+								current[ 2 ] += '"';
+							}
+							current[ 2 ] += '))';
+						}
+						tmpTDALrules = regexp[ 'attributes' ].exec( attributes[ 'data-tdal-attributes' ][ 3 ] );
+					}
+				}
+				if( attributes[ 'data-tdal-omittag' ] && ( regexp[ 'condition' ].exec( attributes[ 'data-tdal-omittag' ][ 3 ] ) ) ) {
+					let tmpValue = ExpressionResultToBoolean( ParsePath( attributes[ 'data-tdal-omittag' ][ 3 ] ) );
+					if( 'true' == tmpValue ) {
+						current[ 1 ] = '';
+						current[ 6 ] = '';
+					} else if( 'false' != tmpValue ) {
+						current[ 0 ] += '+(' + tmpValue + '?"":""';
+						current[ 3 ] = ')' + current[ 3 ];
+						current[ 5 ] += '+(' + tmpValue + '?"":""';
+						current[ 7 ] = ')' + current[ 7 ];
+					}
+				}
+
 				while( null !== ( tmpMatch = regexp[ 'attributesTDAL' ].exec( current[ 1 ] ) ) ) {
 					current[ 1 ] = current[ 1 ].replace( regexp[ 'attributesTDAL' ], '' );
 				}
 			}
 			current[ 1 ] = current[ 1 ].replace( /\s*\/?>$/, '' );
-			if( selfClosed && !( 'undefined' !== typeof closingPosition[ 0 ] ) && ( ( '' != current[ 4 ] ) || ( '' != current[ 3 ] ) || ( '' != current[ 5 ] ) ) ) {
+			if( selfClosed && ( ( '' != current[ 4 ] ) || ( '' != current[ 3 ] ) || ( '' != current[ 5 ] ) ) ) {
 				// if is a tag selfclosed, and if it have contents, i should close it
 				current[ 6 ] = '</' + tmpTDALTags[ 1 ] + '>';
 				selfClosed = false;
